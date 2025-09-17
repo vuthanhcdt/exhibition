1a898afd85c1eed312ae4bdb5e99933b9a3a9374
Cong-Thanh Vu <vuthanh.cdt@gmail.com>
2025-09-17 17:27:51 +0800

    update

--- controller/pid_controller/scripts/collision_detection.py
+++ controller/pid_controller/scripts/collision_detection.py
@@ -23,9 +23,20 @@ class LidarCollisionChecker(Node):
             # Convert PointCloud2 -> numpy (N,3)
             points = pc2.read_points(msg, field_names=('x', 'y', 'z'), skip_nans=True)
             cloud_points = np.array([[p[0], p[1], p[2]] for p in points], dtype=np.float32)
+
+            # Tính khoảng cách Euclidean từ gốc (0,0,0)
+            dists = np.linalg.norm(cloud_points, axis=1)
+
+            # Lọc điểm có khoảng cách trong [0.3, 0.6]
+            mask = (dists > 0.3) & (dists < 0.6)
+            num_points = np.count_nonzero(mask)
+
+            # In/log số điểm tìm được
+            self.get_logger().info(f"Số điểm trong khoảng 0.3 - 0.6 m: {num_points}")
+
         except Exception as e:
             self.get_logger().error(f"Error reading PointCloud data: {e}")
-            return
+
 
 
 def main(args=None):
